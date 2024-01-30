c7e4b76cca8ec3b975cabee84960388d89b75bc4
Amuluru pavan <amulurpavan1@gmail.com>
2024-01-30 17:28:09 +0530

    'tst'

---
+++ math/four.py
@@ -0,0 +1,5 @@
+#Greatest Common Diviser with out math module
+
+import math
+print(math.gcd(100,200,300))
+print(math.gcd(100,50,25,750))
---
+++ math/three.py
@@ -0,0 +1,5 @@
+#print sqrt,ceil,floor
+from math import sqrt,ceil,floor
+print(sqrt(9))
+print(ceil(9.7))
+print(floor(9.8))
